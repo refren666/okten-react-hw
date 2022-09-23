06837512b0dbdda9b133bc310168dd16815e71d9
Refren86 <denis1975shvaiber@gmail.com>
2022-09-24 01:48:42 +0300

    removed header

--- src/services/comments.service.js
+++ src/services/comments.service.js
@@ -3,7 +3,5 @@ import { axiosService } from "./axios.service";
 
 export const commentsService = {
   create: (comment) =>
-    axiosService.post(endpoints.comments, comment, {
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    }),
+    axiosService.post(endpoints.comments, comment),
 };
